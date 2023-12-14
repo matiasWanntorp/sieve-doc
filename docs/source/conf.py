9247422ca1e22ca0a0632aa525b72e9f781724d1
Zelia Bontemps <77738414+LascauxZelia@users.noreply.github.com>
2023-12-14 11:24:57 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'sieve'
+copyright = '2023, ZB'
+author = 'Zelia Bontemps'
 
 release = '0.1'
 version = '0.1.0'
